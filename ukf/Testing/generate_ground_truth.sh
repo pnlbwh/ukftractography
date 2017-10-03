a8fd8d170c7085af31d289f20ecd0a45e4d2cfec
Isaiah Norton <isaiah.norton@gmail.com>
2017-10-03 17:01:39 -0400

    ENH: update generated_ground_truth.sh

--- ukf/Testing/generate_ground_truth.sh
+++ ukf/Testing/generate_ground_truth.sh
@@ -3,39 +3,39 @@
 ## NEEDS TO BE RUN FROM THE test_code FOLDER!
 
 # BINARY
-ukf_path="../../ukf-build/bin/UKFTractography"
+#ukf_path="../../ukf-build/bin/UKFTractography"
 
 # MASK
-mask_path='Data/mask.nhdr'
+mask_path='Data/Input/mask.nhdr'
 
 # SEEDS
-seeds_path='Data/seed.nhdr'
+seeds_path='Data/Input/seed.nhdr'
 
 ## 1T
-diff_im_path='Data/single_tensor.nhdr'
-output_path='Data/1T_fiber.vtk'
+diff_im_path='Data/Input/single_tensor.nhdr'
+output_path='Data/Baseline/1T_fiber.vtk'
 my_command="$ukf_path --dwiFile $diff_im_path --maskFile $mask_path --tracts $output_path --seedsFile $seeds_path 
---seedsPerVoxel 1  --numTensor 1  --numThreads 1 --minBranchingAngle 0.0 --maxBranchingAngle 0.0 --simpleTensorModel --recordFA"
+--seedsPerVoxel 1  --numTensor 1  --numThreads 1 --minBranchingAngle 0.0 --maxBranchingAngle 0.0 --recordNMSE"
 eval $my_command
 
 ## 1T - fw
-diff_im_path='Data/single_tensor_fw.nhdr'
-output_path='Data/1T_fw_fiber.vtk'
+diff_im_path='Data/Input/single_tensor_fw.nhdr'
+output_path='Data/Baseline/1T_fw_fiber.vtk'
 my_command="$ukf_path --dwiFile $diff_im_path --maskFile $mask_path --tracts $output_path --seedsFile $seeds_path 
---seedsPerVoxel 1  --numTensor 1  --numThreads 1 --minBranchingAngle 0.0 --maxBranchingAngle 0.0 --simpleTensorModel --freeWater --recordFreeWater  --recordFA" 
+--seedsPerVoxel 1  --numTensor 1  --numThreads 1 --minBranchingAngle 0.0 --maxBranchingAngle 0.0 --freeWater --recordFreeWater --recordNMSE" 
 eval $my_command
  
 ## 2T
-diff_im_path='Data/two_tensor.nhdr'
-output_path='Data/2T_fiber.vtk'
+diff_im_path='Data/Input/two_tensor.nhdr'
+output_path='Data/Baseline/2T_fiber.vtk'
 my_command="$ukf_path --dwiFile $diff_im_path --maskFile $mask_path --tracts $output_path --seedsFile $seeds_path 
---seedsPerVoxel 1  --numTensor 2  --numThreads 1 --minBranchingAngle 0.0 --maxBranchingAngle 0.0 --simpleTensorModel --recordFA --minFA 0.10 --minGA 0.05 --Qm 0.001 --Ql 10 --Rs 0.015"
+--seedsPerVoxel 1  --numTensor 2  --numThreads 1 --minBranchingAngle 0.0 --maxBranchingAngle 0.0 --minFA 0.10 --minGA 0.05 --Qm 0.001 --Ql 10 --Rs 0.015 --recordNMSE"
 eval $my_command
 # 
 ## 2T - fw
-diff_im_path='Data/two_tensor_fw.nhdr'
-output_path='Data/2T_fw_fiber.vtk'
+diff_im_path='Data/Input/two_tensor_fw.nhdr'
+output_path='Data/Baseline/2T_fw_fiber.vtk'
 my_command="$ukf_path --dwiFile $diff_im_path --maskFile $mask_path --tracts $output_path --seedsFile $seeds_path 
---seedsPerVoxel 1  --numTensor 2  --numThreads 1 --minBranchingAngle 0.0 --maxBranchingAngle 0.0 --simpleTensorModel --freeWater --recordFreeWater --recordFA --minFA 0.10 --minGA 0.05 --Qm 0.01 --Ql 10 --Rs 0.015" 
+--seedsPerVoxel 1  --numTensor 2  --numThreads 1 --minBranchingAngle 0.0 --maxBranchingAngle 0.0 --freeWater --recordFreeWater --recordNMSE --minFA 0.10 --minGA 0.05 --Qm 0.01 --Ql 10 --Rs 0.015" 
 eval $my_command
  

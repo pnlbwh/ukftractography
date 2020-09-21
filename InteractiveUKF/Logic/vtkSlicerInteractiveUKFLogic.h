bff85e84ad721515f32d1c6e032a479d1f0f8805
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2020-09-21 16:59:14 -0400

    STYLE: Replace VTK_OVERRIDE with override
    This commit is required to support with VTK >= 8.90. It fixes
    error like the following:
    
      /tmp/UKFTractography/InteractiveUKF/Logic/vtkSlicerInteractiveUKFLogic.h:52:47: error: expected ‘;’ at end of member declaration
         void PrintSelf(ostream& os, vtkIndent indent) VTK_OVERRIDE;
                                                     ^

--- InteractiveUKF/Logic/vtkSlicerInteractiveUKFLogic.h
+++ InteractiveUKF/Logic/vtkSlicerInteractiveUKFLogic.h
@@ -49,7 +49,7 @@ public:
 
   static vtkSlicerInteractiveUKFLogic *New();
   vtkTypeMacro(vtkSlicerInteractiveUKFLogic, vtkSlicerModuleLogic);
-  void PrintSelf(ostream& os, vtkIndent indent) VTK_OVERRIDE;
+  void PrintSelf(ostream& os, vtkIndent indent) override;
 
   bool InitTractography(vtkMRMLCommandLineModuleNode*);
 
@@ -76,12 +76,12 @@ protected:
   vtkSlicerInteractiveUKFLogic();
   virtual ~vtkSlicerInteractiveUKFLogic();
 
-  virtual void SetMRMLSceneInternal(vtkMRMLScene* newScene) VTK_OVERRIDE;
+  virtual void SetMRMLSceneInternal(vtkMRMLScene* newScene) override;
   /// Register MRML Node classes to Scene. Gets called automatically when the MRMLScene is attached to this logic class.
-  virtual void RegisterNodes() VTK_OVERRIDE;
-  virtual void UpdateFromMRMLScene() VTK_OVERRIDE;
-  virtual void OnMRMLSceneNodeAdded(vtkMRMLNode* node) VTK_OVERRIDE;
-  virtual void OnMRMLSceneNodeRemoved(vtkMRMLNode* node) VTK_OVERRIDE;
+  virtual void RegisterNodes() override;
+  virtual void UpdateFromMRMLScene() override;
+  virtual void OnMRMLSceneNodeAdded(vtkMRMLNode* node) override;
+  virtual void OnMRMLSceneNodeRemoved(vtkMRMLNode* node) override;
 private:
   vtkTrivialProducer* producer;
 
